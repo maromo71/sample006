2a9020997df43dd7d36148b2bfe271c71c5943d3
maromo71 <professormoraes@gmail.com>
2025-05-14 15:27:08 -0300

    Primeiro deploy

---
+++ sample006/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+     <div>
+      <h1 className="text-3xl font-bold mt-4 ml-4">Etec 3DS Tarde</h1>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
+    </div>
+  );
+}
