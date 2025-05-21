f7479d70895bfb0bbbb9ba0c90d590d8de8f93ea
maromo71 <professormoraes@gmail.com>
2025-05-21 14:52:48 -0300

    Teste

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
    <div>
-      <h1 className="text-3xl font-bold mt-4 ml-4">Home</h1>
+      <h1 className="text-3xl font-bold mt-4 ml-4">3 DS</h1>
       <Image
         src="/vercel.svg"
         alt="Vercel Logo"
